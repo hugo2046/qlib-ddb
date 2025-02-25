1ce7ad022d775f37f928dd81aaa37f15297d331b
hugo2046 <shen.lan123@gmail.com>
2025-02-25 11:31:33 +0800

    update

--- qlib/data/backend/ddb_qlib/ddb_client.py
+++ qlib/data/backend/ddb_qlib/ddb_client.py
@@ -2,7 +2,7 @@
 Author: hugo2046 shen.lan123@gmail.com
 Date: 2025-02-19 14:29:41
 LastEditors: hugo2046 shen.lan123@gmail.com
-LastEditTime: 2025-02-25 11:12:40
+LastEditTime: 2025-02-25 11:31:06
 Description: 用于连接ddb
 '''
 from urllib.parse import unquote, urlparse
@@ -48,7 +48,8 @@ class DDBClient:
         session = ddb.session(
             protocol=ddb.settings.PROTOCOL_DDB, 
             show_output=True,  # 使用DDB协议
-            enableASYNC=False
+            enableASYNC=False,
+            reconnect=True
         )
         session.connect(host=host, port=port, userid=user, password=pwd, reconnect=True)
         return session
