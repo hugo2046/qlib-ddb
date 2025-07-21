cacaab36de8153567366acddfd0c2f2b6864cdd2
hugo2046 <shen.lan123@gmail.com>
2025-07-21 21:51:53 +0800

    chore:调整函数结构

--- qlib/data/backend/ddb_qlib/__init__.py
+++ qlib/data/backend/ddb_qlib/__init__.py
@@ -18,8 +18,9 @@ from .ddb_mysql_bridge import DDBMySQLBridge, init_qlib_ddb_from_mysql
 from .schemas import QlibTableSchema
 from .ddb_features import (
     register_ddb_functions_to_qlib,
-    ddb_compute_features,
+    # ddb_compute_features,
     fetch_features_from_ddb,
-    get_query_date_range,
+    normalize_fields_to_ddb,
+    TradeDateUtils,
     adapt_qlib_expr_syntax_for_ddb,
 )
